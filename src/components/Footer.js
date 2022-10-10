70b0e8d606b85ee441b7cc3c3ba7a65628d55747
hamzahhisham306 <hamzahhisham306@gmail.com>
2022-10-10 09:33:32 +0000

    lab16

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div style={{marginBottom:'20px', color:'#393E46', textAlign:'center', fontSize:'20px'}}>
+    All rights reserved © 2022 Hamzah Aldaamas</div>
+      
+  )
+}
+
+export default Footer
